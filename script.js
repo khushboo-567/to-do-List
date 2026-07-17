24fce8a41aa22af3fecde180538b8b52d834a5d6
khushboo-567 <rathorekhushboo567@gmail.com>
2026-07-17 16:49:34 +0530

    Added Local Storage , Task-Counter & Task-filtering

--- script.js
+++ script.js
@@ -21,18 +21,81 @@ addButton.addEventListener("click" , ()=>{
         listItem.appendChild(span);
 
         listContainer.appendChild(listItem);
-
-        span.addEventListener("click", (e) => {
-            e.stopPropagation();
-            listContainer.removeChild(listItem);
-        });
-
-        listItem.addEventListener("click", (e) => {
-            listItem.classList.toggle("checked");
-        });
+        saveTasks();
+        updateTaskCounts();
 
         input.value = "";
     }   
 })
 
+listContainer.addEventListener("click", function(e){
 
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveTasks();
+        updateTaskCounts();
+    }
+
+    else if(e.target.tagName==="SPAN"){
+        e.stopPropagation();
+        e.target.parentElement.remove();
+        saveTasks();
+        updateTaskCounts();
+    }
+
+});
+
+function saveTasks(){
+    localStorage.setItem("tasks", listContainer.innerHTML);
+}
+
+function showTasks(){
+    listContainer.innerHTML = localStorage.getItem("tasks");
+}
+
+const totalTasks = document.getElementById("total-count");
+const completedTasks = document.getElementById("completed-count");
+const remainingTasks = document.getElementById("remaining-count");
+
+function updateTaskCounts(){
+    const Total = listContainer.querySelectorAll("li").length;
+    totalTasks.textContent = `Total: ${Total}`;
+
+    const Completed = listContainer.querySelectorAll("li.checked").length;
+    completedTasks.textContent = `Completed: ${Completed}` ;
+
+    const Remaining = Total - Completed;
+    remainingTasks.textContent = `Remaining: ${Remaining}`;
+}
+
+totalCount = document.getElementById("total-count");
+completedCount = document.getElementById("completed-count");
+remainingCount = document.getElementById("remaining-count");
+
+totalCount.addEventListener("click", showAll); 
+completedCount.addEventListener("click", showCompleted);
+remainingCount.addEventListener("click", showRemaining);
+
+function showRemaining(){
+    tasks=document.querySelectorAll("li");
+    for (const task of tasks){
+        task.classList.contains("checked")?task.style.display="none":task.style.display="block";
+    }
+}
+
+function showCompleted(){
+    tasks=document.querySelectorAll("li");
+    for (const task of tasks){
+        task.classList.contains("checked")?task.style.display="block":task.style.display="none";
+    }
+}
+
+function showAll(){
+    tasks=document.querySelectorAll("li");
+    for (const task of tasks){
+        task.style.display="block";
+    }
+}
+
+showTasks();
+updateTaskCounts();
